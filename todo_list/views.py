1caa6c054b4a53fd0f50ba350cb15f04051d6f8f
Maki Yanagida <myanagida@addu.edu.ph>
2026-09-23 11:17:16 +0800

    Initial commit

---
+++ todo_list/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, redirect
+from .models import List
+
+def home(request):
+    all_items = List.objects.all()
+    context = {'all_items': all_items}
+    return render(request, 'home.html', context)
+
+def about(request):
+    context = {'myname': 'Bob'}
+    return render(request, 'about.html', context)
+
+def delete(request, list_id):
+    item = List.objects.get(pk=list_id)
+    item.delete()
+    return redirect('home')
+
+def strike(request, list_id):
+    item = List.objects.get(pk=list_id)
+    item.completed = True
+    item.save()
+    return redirect('home')
+
+def unstrike(request, list_id):
+    item = List.objects.get(pk=list_id)
+    item.completed = False
+    item.save()
+    return redirect('home')
